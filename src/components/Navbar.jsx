a86168a245ca20e0cc84a76756089b29a76249dd
Jorge Matos <jisapjisap@gmail.com>
2025-10-10 17:45:16 +0200

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img 
+          src="logo.svg"
+          alt="Logo"
+        />
+
+        <ul>
+          {[
+            {label: "Store"},
+            {label: "Mac"},
+            {label: "iPhone"}, 
+            {label: "Watch"},
+            {label: "Vision"},
+            {label: "AirPods"},
+          ].map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
